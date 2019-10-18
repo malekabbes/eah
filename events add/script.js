5d997201014a1a7a35e87d314e639ca4e9e54616
malekabbes665@gmail.com <malekabbes665@gmail.com>
2019-10-18 19:07:08 +0100

    Initial commit

---
+++ events add/script.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    var counter = 0;
+
+    $("#addrow").on("click", function () {
+        var newRow = $("<tr>");
+        var cols = "";
+
+        cols += '<td><input type="text" name="nomevent" class="form-control"' + counter + '"/></td>';
+        cols += '<td><input type="text" name="date" class="form-control"' + counter + '"/></td>';
+        cols += '<td><input type="text" name="place" class="form-control"' + counter + '"/></td>';
+        cols += '<td><input type="file" name="image" class="form-control"' + counter + '"/></td>';
+        cols += '<td><textarea class="form-control" rows="5" id="desc"' + counter + '"/></textarea></td>';
+
+        cols += '<td><input type="button" class="ibtnDel btn btn-md btn-danger "  value="Supprimer"></td>';
+        newRow.append(cols);
+        $("table.order-list").append(newRow);
+        counter++;
+    });
+
+
+
+    $("table.order-list").on("click", ".ibtnDel", function (event) {
+        $(this).closest("tr").remove();       
+        counter -= 1
+    });
+
+
+});
+
+
+
+function calculateRow(row) {
+    var confirmer = +row.find('input[name^="confirmer"]').val();
+
+}
+
+function calculateGrandTotal() {
+    var grandTotal = 0;
+    $("table.order-list").find('input[name^="confirmer"]').each(function () {
+        grandTotal += +$(this).val();
+    });
+    $("#grandtotal").text(grandTotal.toFixed(2));
+}
---
+++ form - register/js/form.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+			$('input[type="file"]').on("change", function () {
+				let filenames = [];
+				let files = document.getElementById("customFile").files;
+				if (files.length > 1) {
+					filenames.push("Total Files (" + files.length + ")");
+				} else {
+					for (let i in files) {
+						if (files.hasOwnProperty(i)) {
+							filenames.push(files[i].name);
+						}
+					}
+				}
+				$(this)
+					.next(".custom-file-label")
+					.html(filenames.join(","));
+			});
+		});
